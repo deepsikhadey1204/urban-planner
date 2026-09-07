b1c0f0a646c7a4abec6750a58b02e66b2cec159f
Deepsikha Dey <deepsikha.dey@esri.in>
2026-09-07 15:19:36 +0530

    Adding map tools

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,22 +1,38 @@
-import { useRef, useEffect, useState } from "react"
-import Map from "@arcgis/core/Map"
-import MapView from "@arcgis/core/views/MapView"
+import { useRef, useEffect, useState } from "react";
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import Home from "@arcgis/core/widgets/Home.js";
 
 export default function MapPage(){
-    const mapRef = useRef(null)
+    const mapRef = useRef(null);
+    const [mapView, setMapView] = useState(null);
 
     useEffect(() => {
         const map = new Map({
             basemap: "satellite"
         });
 
-        const mapview = new MapView({
+        const view = new MapView({
             map: map,
             container: mapRef.current,
             center: [78, 28],
             zoom: 4
-        })
-    }, [])
+        });
+
+        setMapView(view);
+    }, []);
+
+    useEffect(() => {
+
+        if(!mapView) return;
+
+        const homeWidget = new Home({
+            view: mapView
+        });
+
+        mapView.ui.add(homeWidget, "top-left");
+
+    }, [mapView]);
 
     return(
         <div id="mapDiv" ref={mapRef}></div>
